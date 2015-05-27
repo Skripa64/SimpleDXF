c65970f248c21d64e5e5749ae81e5cc81c07033f
Skripa64 <ivan@skripkarev.ru>
2015-05-27 22:30:07 +0300

    Add OpenTK

--- SimpleDXF/Form1.Designer.cs
+++ SimpleDXF/Form1.Designer.cs
@@ -28,12 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.glControl1 = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl1.Location = new System.Drawing.Point(0, 0);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(704, 431);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 431);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private OpenTK.GLControl glControl1;
     }
 }
 
--- SimpleDXF/Form1.cs
+++ SimpleDXF/Form1.cs
@@ -7,14 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 
 namespace SimpleDXF
 {
     public partial class Form1 : Form
     {
+        bool loaded = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void SetupViewport()
+        {
+            int w = glControl1.Width;
+            int h = glControl1.Height;
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, w,0,h,-1,1);
+            GL.Viewport(0,0,w,h);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+
+            GL.ClearColor(Color.White);
+            SetupViewport();
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            if (!loaded) return;
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded) return;
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Color3(Color.Black);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex2(0, 0);
+            GL.Vertex2(100, 100);
+            GL.End();
+
+            glControl1.SwapBuffers();
         }
     }
 }
